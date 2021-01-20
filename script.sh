86a0a4208cba9a2811db5586736b2d518c34ccba
Suryaprakash Vengadesan <sury@berkeley.edu>
2021-01-20 10:41:26 -0800

    new script and 195 notes

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp ~/Documents/classes/math195/lecture_notes.pdf ./files/math195.pdf
+cp ~/Documents/classes/math199/analysis_notes.pdf ./files/math199_notes.pdf
+cp ~/Documents/classes/math199/hw.pdf ./files/math199_hw.pdf
